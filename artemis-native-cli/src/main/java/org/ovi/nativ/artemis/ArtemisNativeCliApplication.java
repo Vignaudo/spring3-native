1f0fa7f7fc283fce09f5cbc0b68ffdb512fa6fbe
Olivier Vignaud <ovi@ubiqube.com>
2025-02-18 10:21:23 +0100

    📄 Update license headers.

--- artemis-native-cli/src/main/java/org/ovi/nativ/artemis/ArtemisNativeCliApplication.java
+++ artemis-native-cli/src/main/java/org/ovi/nativ/artemis/ArtemisNativeCliApplication.java
@@ -1,5 +1,5 @@
 /**
- *     Copyright (C) 2023-2024 Olivier Vignaud.
+ *     Copyright (C) 2023-2025 Olivier Vignaud.
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
